089bbfc6010765785ca311224c5b07ff430dfe6d
User <User@192.168.1.42>
2024-08-14 13:46:59 +0530

    Initial Commit.

---
+++ com.example.demo/src/main/java/com/example/com/example/demo/UserService.java
@@ -0,0 +1,22 @@
+package com.example.com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	@Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+}
